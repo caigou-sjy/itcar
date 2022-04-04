31e62afbf0bdf3c4db9d654d4ef37f5aa3139d30
apple-sujiaying <13399162213@163.com>
2022-04-05 00:25:42 +0800

    背景图+user属性

--- it-user/src/main/java/com/sjy/it/authority/entity/User.java
+++ it-user/src/main/java/com/sjy/it/authority/entity/User.java
@@ -1,5 +1,6 @@
 package com.sjy.it.authority.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.apache.ibatis.type.Alias;
 
 import lombok.AllArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Alias("User")
 @NoArgsConstructor
@@ -14,6 +16,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "user_tab")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
@@ -28,5 +31,28 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "user_role")
     private Role role;
+
+    @Column(name = "user_age")
+    private Integer age;
+
+    @Column(name = "user_gender")
+    private String gender;
+
+    @Column(name = "user_phone")
+    private String phone;
+
+    @Column(name = "user_birth")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone="GMT+8")
+    private Date birth;
+
+    @Column(name = "user_address")
+    private String address;
+
+    @Column(name = "user_picture")
+    private String picture;
+
+    @Column(name = "user_createtime")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone="GMT+8")
+    private Date createtime;
 
 }
